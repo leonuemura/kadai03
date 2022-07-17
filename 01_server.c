c2921a0b049cbaab24d30de7d5c89851cf977164
leonuemura <106325619+leonuemura@users.noreply.github.com>
2022-07-17 14:45:55 +0900

    Update 01_server.c

--- 01_server.c
+++ 01_server.c
@@ -1,6 +1,46 @@
 #include "watalab.h"
 #define BUFSIZE 1024
 
+int watalab_tcp_listen(int port)
+{
+  int sock;
+  struct sockaddr_in addr;
+  int yes = 1;
+  int ret;
+  sock = socket(AF_INET, SOCK_STREAM, 0);
+  if(sock < 0) {
+    perror("socket");
+    exit(-1);
+  }
+  
+  bzero((char *) &addr, sizeof(addr));
+  addr.sin_family = AF_INET;
+  addr.sin_addr.s_addr = htonl(INADDR_ANY);
+  addr.sin_port = htons(port);
+  setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(yes));
+  ret = bind(sock, (struct sockaddr *)&addr, sizeof(addr));
+  if(ret < 0) {
+    perror("bind");
+    exit(-1);
+  }
+  ret = listen(sock, 1024);
+  if(ret < 0) {
+    perror("listen");
+    close(sock);
+    exit(-1);
+  }
+  return sock;
+}
+
+int watalab_accept(int sock_listen){
+  struct sockaddr addr;
+  int len;
+  int ret;
+  len = sizeof(struct sockaddr);
+  ret = accept(sock_listen, &addr, (socklen_t*) &len);
+  return ret;
+}
+
 int watalab_do_talk(int sock)
 {
   fd_set fds;

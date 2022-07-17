27acf87498df720033ede2b2956e3b971b927c07
leonuemura <106325619+leonuemura@users.noreply.github.com>
2022-07-17 14:53:26 +0900

    Update 02_client.c

--- 02_client.c
+++ 02_client.c
@@ -20,6 +20,36 @@ int watalab_tcp_connect(const char *hostname, int port)
   }
 }
 
+int watalab_do_talk(int sock)
+{
+  fd_set fds;
+  char buf[1024];
+  
+  FD_ZERO(&fds);
+  FD_SET(0, &fds);
+  FD_SET(sock, &fds);
+  
+  select(sock + 1, &fds, NULL, NULL, NULL);
+  
+  if(FD_ISSET(0, &fds) != 0){
+    int ret;
+    ret = read(0, buf, BUFSIZE);
+    write(sock, buf, ret);
+  }
+  
+  if(FD_ISSET(sock, &fds) != 0){
+    int ret;
+    ret = recv(sock, buf, BUFSIZE, 0);
+    if(ret > 0){
+      write(1, buf, ret);
+    }else{
+      return -1;
+    }
+  }
+  
+  return 1;
+}
+
 
 
 int main(int argc, char** argv)

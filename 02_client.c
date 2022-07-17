1c42338d9904b229ec4b3f887074d30a30ce6dbf
leonuemura <106325619+leonuemura@users.noreply.github.com>
2022-07-17 14:52:36 +0900

    Update 02_client.c

--- 02_client.c
+++ 02_client.c
@@ -1,6 +1,25 @@
 #include "watalab.h"
 #define BUFSIZE 1024
 
+int watalab_tcp_connect(const char *hostname, int port)
+{
+  int sock;
+  int ret;
+  struct sockaddr_in addr;
+  struct hostent *host;
+  sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+  addr.sin_family = AF_INET;
+  host = gethostbyname(hostname);
+  addr.sin_addr = *(struct in_addr *)(host->h_addr_list[0]);
+  addr.sin_port = htons(port);
+  ret = connect(sock, (struct sockaddr *)&addr, sizeof addr);
+  if(ret < 0){
+    return -1;
+  }else{
+    return sock;
+  }
+}
+
 
 
 int main(int argc, char** argv)
